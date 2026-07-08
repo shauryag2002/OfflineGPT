a6ccb402c8ead4f90b9762aa004377643b5214c5
Shaurya Gupta <guptashaurya2002@gmail.com>
2026-07-08 14:26:13 +0530

    Removed: Boilerplate code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,45 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
-
+import { Host, ModalBottomSheetRef, Text } from "@expo/ui/jetpack-compose";
+import { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
+  const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
+  const bottomSheetRef = useRef<ModalBottomSheetRef>(null);
+  const toggleBottomSheet = () => {
+    // setBottomSheetVisible(!isBottomSheetVisible);
+    if (isBottomSheetVisible) {
+      bottomSheetRef.current?.hide(); // Hide the bottom sheet
+    } else {
+      bottomSheetRef.current?.expand(); // Show the bottom sheet
+    }
+    setBottomSheetVisible(!isBottomSheetVisible);
+  };
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+    <SafeAreaView>
+      <Host matchContents>
+        <Text color="yellow" style={{ background: "black" }}>
+          Welcome to OfflineGPT
+        </Text>
+        {/* <Button onClick={toggleBottomSheet}>
+          <Text>Open fking bottom sheet.</Text>
+        </Button> */}
+        {/* <LoadingIndicator /> */}
+        {/* <ModalBottomSheet
+          properties={{ shouldDismissOnClickOutside: false }}
+          ref={bottomSheetRef}
+          onDismissRequest={toggleBottomSheet}
+        >
+          <Column
+            verticalArrangement={{ spacedBy: 12 }}
+            modifiers={[paddingAll(24)]}
+          >
+            <Text>Hello from bottom sheet!</Text>
+            <Text>You can add more content here.</Text>
+            <Button onClick={toggleBottomSheet}>
+              <Text>Close</Text>
+            </Button>
+          </Column>
+        </ModalBottomSheet> */}
+      </Host>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
